49936b280736f51d9ce402a62799e0610d82ac75
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-10-09 13:38:10 -0700

    added an outline for the return of ApodContainer

---
+++ src/components/ApodContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ApodContainer(props){
+    return(
+        <div className="apod-container">
+            <picture/>
+            <title/>
+            <date/>
+            <explanation/>
+        </div>
+    );
+}
