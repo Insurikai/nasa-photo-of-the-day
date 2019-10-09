9fb025ff48e9a9d002a391999823f9685d2b6ea6
Insurikai <40343703+Insurikai@users.noreply.github.com>
2019-10-09 13:29:26 -0700

    set default state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React,{useState, useEffect} from "react";
 import "./App.css";
 
 function App() {
+  const [apod, setApod] = useState({});
+  
   return (
     <div className="App">
       <p>
